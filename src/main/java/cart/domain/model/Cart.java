08d9c7ad9ea02beb17d44de68025736ad22bcfdd
Marcos653 <marcos.costas@xbrain.com.br>
2024-02-11 17:49:10 -0300

    refactor: change the position

--- src/main/java/cart/domain/model/Cart.java
+++ src/main/java/cart/domain/model/Cart.java
@@ -1,10 +1,7 @@
 package cart.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
